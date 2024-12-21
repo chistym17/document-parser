d985f97bc467cb3bdb5e2378aaae5a5c7e7048bc
chistym17 <mchisty31@gmail.com>
2024-12-21 22:26:08 +0600

    upload section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from '@/components/Hero';
+import FileUpload from '@/components/FileUpload';
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
+    <main className="min-h-screen">
+      <section className="h-screen">
+        <Hero />
+      </section>
+      <section className="h-screen bg-gray-50">
+        <FileUpload />
+      </section>
     </main>
   );
 }
